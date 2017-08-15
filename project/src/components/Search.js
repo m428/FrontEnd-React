0c6e393d977d8db6dfc3aed2c7b07d1da8b1830c
Mala Nimalasuriya <m428@users.noreply.github.com>
2017-08-15 06:26:59 -0700

    update search styles

--- project/src/components/Search.js
+++ project/src/components/Search.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './Search.css';
-
+import FaSearch from 'react-icons/lib/fa/search';
 
 class Search extends React.Component {
    render() {
       return (
-         <div className={styles.searchBox}>
-            Search
-         </div>
+        <div className={styles.searchBox}>
+           <div className={styles.search}><FaSearch className={styles.searchIcon}/>search</div>
+        </div>
       );
    }
 }
