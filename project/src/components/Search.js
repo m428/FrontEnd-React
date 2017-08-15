1938aee1dba0f5cca7e8a962e945188134d05a34
Mala Nimalasuriya <m428@users.noreply.github.com>
2017-08-15 12:09:50 -0700

    update search field

--- project/src/components/Search.js
+++ project/src/components/Search.js
@@ -7,7 +7,7 @@ class Search extends React.Component {
    render() {
       return (
         <div className={styles.searchBox}>
-           <div className={styles.search}><FaSearch className={styles.searchIcon}/>search</div>
+           <div className={styles.search}><FaSearch className={styles.searchIcon}/><input type="text" className={styles.search} value="Search"/></div>
         </div>
       );
    }
